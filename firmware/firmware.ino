9e703aa90c2db29a6c0e060abcaecfdf2fffae3f
shycherry <vinpowful@free.fr>
2015-09-01 22:34:15 +0200

    wip

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,4 +1,5 @@
 #define DEBUG
+#define NRF24BRIDGE_TEST
 
 #include <SPI.h>
 #include "nRF24L01.h"
@@ -28,7 +29,7 @@ const int lights[NB_LIGHTS] = {
   NRF_LIGHT_GREEN,
   NRF_LIGHT_BLUE
 };
-
+//
 int brightnesses[NB_LIGHTS] = {128, 128, 128, 128, 128};
 
 // Set up nRF24L01 radio on SPI bus plus pins 9 & 10
@@ -42,27 +43,7 @@ RF24 radio(9,10);
 const uint64_t pipes[2] = {
   0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
-//
-// Role management
-//
-// Set up role.  This sketch uses the same software for all the nodes
-// in this system.  Doing so greatly simplifies testing.
-//
-
-// The various roles supported by this sketch
-typedef enum {
-  role_ping_out = 1, role_pong_back }
-role_e;
-
 bool isGlowTestActive = true;
-
-// The debug-friendly names of those roles
-const char* role_friendly_name[] = {
-  "invalid", "Ping out", "Pong back"};
-
-// The role of the current running sketch
-role_e role = role_pong_back;
-
 int brightness = 0;
 unsigned long time = 0;
 
@@ -112,8 +93,6 @@ void setup(void)
   Serial.begin(57600);
   printf_begin();
   printf("\n\rRF24/examples/GettingStarted/\n\r");
-  printf("ROLE: %s\n\r",role_friendly_name[role]);
-  printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
 #endif
 
   //
@@ -141,7 +120,7 @@ void setup(void)
 
   // optionally, reduce the payload size.  seems to
   // improve reliability
-  radio.setPayloadSize(4);
+  radio.setPayloadSize(8);
 
   //
   // Open pipes to other nodes for communication
@@ -152,16 +131,13 @@ void setup(void)
   // Open 'our' pipe for writing
   // Open the 'other' pipe for reading, in position #1 (we can have up to 5 pipes open for reading)
 
-  if ( role == role_ping_out )
-  {
+#ifdef NRF24BRIDGE_TEST
     radio.openWritingPipe(pipes[0]);
     radio.openReadingPipe(1,pipes[1]);
-  }
-  else
-  {
+#else
     radio.openWritingPipe(pipes[1]);
     radio.openReadingPipe(1,pipes[0]);
-  }
+#endif
 
   //
   // Start listening
@@ -179,10 +155,19 @@ void setup(void)
 
 void loop(void)
 {
+#ifdef NRF24BRIDGE_TEST
+  radio.stopListening();
+  unsigned long time = millis();
+  printf("Now sending %lu...", time);
+  bool ok = radio.write( &time, sizeof(unsigned long));
+  radio.startListening();
+  delay(1000);
+#else
   refresh_lights();
 
   if(isGlowTestActive)
     update_glow_test();
+#endif
 }
 
 void check_radio(void)
@@ -194,53 +179,51 @@ void check_radio(void)
   // Have we successfully transmitted?
   if ( tx )
   {
-    if ( role == role_ping_out )
-      printf("Send:OK\n\r");
-
-    if ( role == role_pong_back )
-      printf("Ack Payload:Sent\n\r");
+#ifdef NRF24BRIDGE_TEST
+    printf("Send:OK\n\r");
+#else
+    printf("Ack Payload:Sent\n\r");
+#endif
   }
 
   // Have we failed to transmit?
   if ( fail )
   {
-    if ( role == role_ping_out )
-      printf("Send:Failed\n\r");
-
-    if ( role == role_pong_back )
-      printf("Ack Payload:Failed\n\r");
+#ifdef NRF24BRIDGE_TEST
+    printf("Send:Failed\n\r");
+#else
+    printf("Ack Payload:Failed\n\r");
+#endif
   }
 
   // Transmitter can power down for now, because
   // the transmission is done.
-  if ( ( tx || fail ) && ( role == role_ping_out ) )
+#ifdef NRF24BRIDGE_TEST
+  if ( ( tx || fail ))
     radio.powerDown();
+#endif
 
   // Did we receive a message?
   if ( rx )
   {
     // If we're the sender, we've received an ack payload
-    if ( role == role_ping_out )
-    {
-      int message_count = 0;
-      radio.read(&message_count,sizeof(message_count));
-      printf("Ack:%lu\n\r",message_count);
-    }
-
+#ifdef NRF24BRIDGE_TEST
+    unsigned long got_time = 0;
+    radio.read(&got_time,sizeof(unsigned long));
+    printf("Ack:%lu\n\r",got_time);
+#else
     // If we're the receiver, we've received a time message
-    if ( role == role_pong_back )
-    {
-      // Get this payload and dump it
-      static unsigned long got_time;
-      radio.read( &got_time, sizeof(got_time) );
-      printf("Got payload %lu\n\r",got_time);
+    // Get this payload and dump it
+    unsigned long got_time;
+    radio.read( &got_time, sizeof(unsigned long) );
+    printf("Got payload %lu\n\r",got_time);
 
-      isGlowTestActive = !isGlowTestActive;
+    isGlowTestActive = !isGlowTestActive;
 
-      // Add an ack packet for the next time around.  This is a simple
-      // packet counter
-      radio.writeAckPayload( 1, &got_time, sizeof(got_time) );
-    }
+    // Add an ack packet for the next time around.  This is a simple
+    // packet counter
+    radio.writeAckPayload( 1, &got_time, sizeof(unsigned long) );
+#endif
   }
 }
 
